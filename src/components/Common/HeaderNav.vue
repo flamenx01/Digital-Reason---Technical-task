9d03aa8c58c4b696d4636f22245acd1313e9efdd
Scott Mason <smason@oes.com>
2024-01-07 16:23:15 +0000

    added mobile nav menu

--- src/components/Common/HeaderNav.vue
+++ src/components/Common/HeaderNav.vue
@@ -5,21 +5,32 @@
         <img src="https://www.goodthingsfoundation.org/wp-content/uploads/2021/01/good-things-foundation-logo.png" alt="Branding Logo">
       </router-link >
     </div>
-    <nav class="header_nav">
+    <nav class="header-nav nav-menu">
       <ul>
         <li v-for="(item, index) in store.menuItems" :key="index">
           <router-link :to="item.url">{{item.title}}</router-link>
         </li>
       </ul>
     </nav>
+    <button @click="showMenu = !showMenu" class="nav-button btn btn--alt-2">Menu</button>
   </div>
+  <nav v-if="showMenu" class="mobile-nav nav-menu container">
+    <ul>
+      <li v-for="(item, index) in store.menuItems" :key="index">
+        <router-link :to="item.url">{{item.title}}</router-link>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script setup>
 import { useThemeStore } from '@/stores/store';
-
+import { ref } from "vue"
+const windowWidth = ref(window.innerWidth)
 const store = useThemeStore();
 if (!store.menuItems.length) store.getMenu();
+
+const showMenu = ref(false);
 </script>
 
 <style lang="scss" scoped>
@@ -28,6 +39,7 @@ if (!store.menuItems.length) store.getMenu();
   justify-content: space-between;
   align-items: center;
   border-top: 1px solid #ccc;
+  min-height: 5.75rem;
   .header_branding {
     &__logo {
       display: block;
@@ -40,31 +52,66 @@ if (!store.menuItems.length) store.getMenu();
       }
     }
   }
-  .header_nav {
+  .header-nav {
     display: flex;
     ul {
-      display: flex;
       li {
-        list-style: none;
-        margin-left: 0;
-        margin-top: 0;
-        padding: 2.188rem 0.625rem;
+        padding: 2rem 0.625rem;
         a {
           text-decoration: none;
-          font-size: 1.1rem;
-          font-weight: 500;
+        }
+      }
+    }
+  }
+  .nav-button {
+    display: none;
+  }
+  @media (max-width: 70rem) {
+    .nav-button {
+      display: block;
+    }
+    .header-nav {
+      display: none;
+    }
+    .mobile-nav {
+      display: flex;
+    }
+  }
+}
+.nav-menu {
+    ul {
+    display: flex;
+    list-style: none;
+    margin-left: 0;
+    margin-top: 0;
+    li {
+      margin-left: 0;
+      margin-top: 0;
+            a {
+        font-size: 1.1rem;
+        font-weight: 500;
+        color: $dark-text;
+        &:visited {
           color: $dark-text;
-          &:visited {
-            color: $dark-text;
-          }
-          &:hover {
-            color: $main-color;
-          }
+        }
+        &:hover {
+          color: $main-color;
         }
       }
     }
   }
 }
-
-
+.mobile-nav {
+  display: none;
+  ul {
+    flex-direction: column;
+    li {
+      list-style: none;
+      padding: 1rem 0.625rem;
+    }
+  }
+  @media (max-width: 70rem) {
+    display: flex;
+  }
+}
 </style>
