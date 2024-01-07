c99a68b0dd86c297e9fd4ba28bf50fb8284c21ff
Scott Mason <smason@oes.com>
2024-01-07 15:03:04 +0000

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <the-header />
   <router-view></router-view>
+  <the-footer />
 </template>
 
 <script setup>
 import theHeader from '@/components/Common/TheHeader.vue';
+import theFooter from '@/components/Common/TheFooter.vue';
 </script>
 
 <style lang="scss">
---
+++ src/components/Common/TheFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <div class="footer container">
+      Copyright {{year}} © 			
+    </div>
+  </footer>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const date = new Date();
+const year = ref(date.getFullYear());
+
+</script>
+
+<style lang="scss" scoped>
+  .footer {
+    background-color: $grey-color;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+    display: flex;
+    justify-content: center;
+  }
+
+</style>
