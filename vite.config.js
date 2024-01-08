4c6b6fed89d9b6af849bc27dea2281506e77d6fd
Scott Mason <smason@oes.com>
2024-01-08 11:56:54 +0000

    added cleaner way of handling alias

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import * as path from 'path';
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: [
-        { find: '@', replacement: path.resolve(__dirname, 'src') },
-        { find: 'HOMEFOLDER', replacement: path.resolve(__dirname, 'src') },
-    ],
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
   },
   // Inject SASS variables into all scss files
   css: {
